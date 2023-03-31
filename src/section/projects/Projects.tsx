f0ea4b0d2d25a8d049507aa0e7c0378cd9eaac1e
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-03-31 22:44:55 +0500

    changes in project section

--- src/section/projects/Projects.tsx
+++ src/section/projects/Projects.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion, spring } from "framer-motion";
 
 const Projects = () => {
   return (
@@ -11,25 +12,48 @@ const Projects = () => {
           My Projects
         </h2>
         <div className="h-full flex justify-center flex-col pb-28">
-          <p className="font-thin text-lg text-white ">
+          <motion.p
+            className="font-thin text-lg text-white "
+            initial={{ opacity: 0, translateY: "10vw" }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 0.75 }}
+          >
             In this section, you can find some of the projects that I have
             developed as a software developer. These projects showcase my skills
             and expertise in designing and developing applications using a range
             of technologies. Each project has been carefully crafted to meet the
             unique needs of the end-users, and I have strived to deliver
             high-quality work that is both functional and visually appealing.
-          </p>
-          <p className="font-thin text-lg text-white">
-            <span className="text-red-600 font-medium text-2xl">
-              Note:&nbsp;
-            </span>
-            I am unable to showcase all of my projects due to client privacy
+          </motion.p>
+          <motion.span
+            className="text-red-600 font-medium text-4xl"
+            initial={{ opacity: 0, translateX: "-20vw" }}
+            whileInView={{ opacity: 1, translateX: 0 }}
+            transition={{
+              //   duration: 0.3,
+              type: "spring",
+              stiffness: 1000,
+              damping: 200,
+            }}
+            style={{
+              fontFamily: "Sirin Stencil",
+            }}
+          >
+            Note:&nbsp;
+          </motion.span>
+          <motion.p
+            className="font-thin text-lg text-white"
+            initial={{ opacity: 0, translateY: "10vw" }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            As, I am unable to showcase all of my projects due to client privacy
             policies, I can assure you that my portfolio represents a
             comprehensive overview of my skills and expertise as a developer.
             <br />
             <br />
             As well as some of these are not my designs but the code is mine.
-          </p>
+          </motion.p>
         </div>
       </div>
       <div></div>
